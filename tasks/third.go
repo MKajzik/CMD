c924a6f408985287f8bad2d9586aab1bc8cab64a
Mateusz Kajzik <mkajzik@virtuslab.com>
2022-04-28 00:27:48 +0200

    Fix third task

--- tasks/third.go
+++ tasks/third.go
@@ -1,10 +1,8 @@
 package tasks
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"os"
 	"os/exec"
 	"strings"
 	"time"
@@ -18,18 +16,15 @@ func Third(done chan bool) {
 	text.Write("Trzecie zadanie.\nUtwórz katalog \"tmp/files/secret\". \nPamiętaj, że foldery tmp i files nie istnieją i musisz je osobno stworzyć.")
 	text.Write("Podpowiedź: Da się to zrobić jedną komendą")
 	for {
-		fmt.Print("$ ")
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		input := scanner.Text()
+		text.Write("Naciśnij ENTER kiedy wykonasz zadanie")
+		fmt.Scanln()
 
-		out, err := exec.Command("sh", "-c", input).Output()
+		out, err := exec.Command("sh", "-c", "[ -d \"./path/dir/\" ] && echo \"Directory /path/dir/ exists.\"").Output()
 		if err != nil {
 			log.Fatal(err)
 		}
-		text.Write(fmt.Sprintf("Output: %s", out))
 
-		if strings.TrimSuffix(input, "\n") == "pwd" {
+		if strings.TrimSuffix(string(out), "\n") == "Directory /path/dir/ exists." {
 			text.Write("Brawo udało Ci się ukończyć drugie zadanie.")
 			break
 		}
